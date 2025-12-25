f6f9f8f241622e9cffd551f3ceda890fdb65d320
Roger Filomeno <rpfilomeno@gmail.com>
2025-12-26 00:04:05 +0800

    initial

---
+++ src/content/config.ts
@@ -0,0 +1,16 @@
+import { defineCollection, z } from 'astro:content'
+
+const blog = defineCollection({
+	// Type-check frontmatter using a schema
+	schema: ({ image }) =>
+		z.object({
+			title: z.string(),
+			description: z.string(),
+			pubDate: z.coerce.date(),
+			updatedDate: z.coerce.date().optional(),
+			coverImageCredit: z.string().optional(),
+			cover: image().optional(),
+		}),
+})
+
+export const collections = { blog }
