0b6d59d5ecbf83acac10e4a190bf9015b5b71595
Roger Filomeno <rpfilomeno@gmail.com>
2025-12-26 01:20:27 +0800

    feat: initialize blog homepage with site constants, content, and logo

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Saral Theme'
+export const SITE_TITLE = 'A.I. Sloth Slayer'
 export const SITE_DESCRIPTION =
 	'A simple theme for personal blog sites, created for Astro framework'
 
@@ -13,19 +13,19 @@ export const SITE_BASE = ''
 export const SocialLinks = [
 	{
 		name: 'GitHub',
-		url: 'https://github.com/yashjawale',
+		url: 'https://github.com/rpfilomeno',
 	},
 	{
 		name: 'LinkedIn',
-		url: 'https://www.linkedin.com/in/yashjawale/',
+		url: 'https://www.linkedin.com/in/rpfilomeno/',
 	},
 	{
 		name: 'X / Twitter',
-		url: 'https://twitter.com/imyashjawale',
+		url: 'https://twitter.com/godie',
 	},
 	{
-		name: 'Instagram',
-		url: 'https://instagram.com/imyashjawale',
+		name: 'YouTube',
+		url: 'https://www.youtube.com/@AISlothSlayer',
 	},
 ]
 
