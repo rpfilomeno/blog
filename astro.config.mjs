5c74cb834cf369141156a96b71cbfdf6cd38dff2
Roger Filomeno <rpfilomeno@gmail.com>
2025-12-28 17:12:54 +0800

    chore: Update Astro site URL to roger.rogverse.fyi

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ import { remarkModifiedTime } from './src/plugins/remark-modified-time.mjs'
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://yashjawale.github.io',
+	site: 'https://roger.rogverse.fyi',
 	base: '',
 	build: {
 		format: 'file',
